0fc554c17a30b62d4b364aef1c423a72aa1fbe67
glbart <hiccup.colin@yandex.ru>
2025-05-07 01:05:28 +0300

    added reading programs from files and fixe incorrect handling of some opcodes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 #[derive(Debug)]
 struct CPU {
     registers: [u8; 16],
@@ -15,6 +17,39 @@ impl CPU {
         let op_byte2 = self.memory[p + 1] as u16;
 
         op_byte1 << 8 | op_byte2
+    }
+
+    fn run(&mut self) {
+        loop {
+            let opcode = self.read_opcode();
+            println!("instruction: {:x}", opcode);
+            self.position_in_memory += 2;
+
+            let c = ((opcode >> 12) & 0x000F) as u8;
+            let x = ((opcode >> 8) & 0x000F) as u8;
+            let y = ((opcode >> 4) & 0x000F) as u8;
+            let d = (opcode & 0x000F) as u8;
+
+            let nnn = opcode & 0x0FFF;
+            let kk = (opcode & 0x00FF) as u8;
+
+            match (c, x, y, d) {
+                (0, 0, 0, 0) => {
+                    return;
+                }
+                (0, 0, 0xE, 0) => self.display.clear(),
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x1, _, _, _) => self.jmp_to_addr(nnn),
+                (0x2, _, _, _) => self.call(nnn),
+                (0x3, _, _, _) => self.skip_if_eq(x, kk),
+                (0x4, _, _, _) => self.skip_if_neq(x, kk),
+                (0x5, _, _, 0) => self.skip_if_eq_registers(x, y),
+                (0x6, _, _, _) => self.load_in_register(x, kk),
+                (0x7, _, _, _) => self.add_xkk(x, kk),
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                _ => todo!("opcode: {:04x}", opcode),
+            }
+        }
     }
 
     fn call(&mut self, addr: u16) {
@@ -40,54 +75,18 @@ impl CPU {
         self.position_in_memory = call_addr as usize;
     }
 
-    fn run(&mut self) {
-        loop {
-            let opcode = self.read_opcode();
-            self.position_in_memory += 2;
-
-            let c = ((opcode >> 12) & 0x000F) as u8;
-            let x = ((opcode >> 8) & 0x000F) as u8;
-            let y = ((opcode >> 4) & 0x000F) as u8;
-            let d = (opcode & 0x000F) as u8;
-
-            let nnn = opcode & 0x0FFF;
-            let kk = (opcode & 0x00FF) as u8;
-
-            match (c, x, y, d) {
-                (0, 0, 0, 0) => {
-                    return;
-                }
-                (0, 0, 0xE, 0) => self.display.clear(),
-                (0, 0, 0xE, 0xE) => self.ret(),
-                (0x1, _, _, _) => self.jmp_to_addr(nnn),
-                (0x2, _, _, _) => self.call(nnn),
-                (0x3, _, _, _) => self.skip_if_eq(x, kk),
-                (0x4, _, _, _) => self.skip_if_neq(x, kk),
-                (0x5, _, _, 0) => self.skip_if_eq_registers(x, y),
-                (0x6, _, _, _) => self.load_in_register(c, kk),
-                (0x7, _, _, _) => self.add_xkk(x, kk),
-                (0x8, _, _, 0x4) => self.add_xy(x, y),
-                _ => todo!("opcode: {:04x}", opcode),
-            }
-        }
-    }
-
-    fn load_in_register(&mut self, x: u8, kk: u8) {
-        self.registers[x as usize] = self.memory[kk as usize];
-    }
-
     fn jmp_to_addr(&mut self, addr: u16) {
         self.position_in_memory = addr as usize;
     }
 
     fn skip_if_eq(&mut self, x: u8, kk: u8) {
-        if self.registers[x as usize] == self.memory[kk as usize] {
+        if self.registers[x as usize] == kk {
             self.position_in_memory += 2;
         }
     }
 
-    fn skip_if_neq(&mut self, r: u8, addr: u8) {
-        if self.registers[r as usize] != self.memory[addr as usize] {
+    fn skip_if_neq(&mut self, r: u8, kk: u8) {
+        if self.registers[r as usize] != kk {
             self.position_in_memory += 2;
         }
     }
@@ -98,11 +97,16 @@ impl CPU {
         }
     }
 
+    fn load_in_register(&mut self, x: u8, kk: u8) {
+        self.registers[x as usize] = kk;
+        println!("load in reg: {}", kk);
+        println!("reg[{}] = {}", x, self.registers[x as usize]);
+    }
+
     fn add_xkk(&mut self, x: u8, kk: u8) {
         let arg1 = self.registers[x as usize];
-        let arg2 = self.memory[kk as usize];
 
-        self.registers[x as usize] = arg1 + arg2;
+        self.registers[x as usize] = arg1 + kk;
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
@@ -143,29 +147,21 @@ fn main() {
     let mut cpu = CPU {
         registers: [0; 16],
         memory: [0; 4096],
-        position_in_memory: 0,
+        position_in_memory: 512,
         stack: [0; 16],
         stack_pointer: 0,
         display: Display::new(),
     };
 
-    cpu.registers[0] = 5;
-    cpu.registers[1] = 10;
+    //let program = fs::read("add.ch8").unwrap();
+    let program = fs::read("cond.ch8").unwrap();
+    let program_len = program.len();
 
     let mem = &mut cpu.memory;
-    mem[0] = 0x21;
-    mem[1] = 0x00;
-    mem[2] = 0x21;
-    mem[3] = 0x00;
-    mem[4] = 0x00;
-    mem[5] = 0x00;
 
-    let add_twice = [0x80, 0x14, 0x80, 0x14, 0x00, 0xEE];
-
-    mem[0x100..0x106].copy_from_slice(&add_twice);
+    mem[512..512 + program_len].copy_from_slice(&program);
 
     cpu.run();
-    //assert_eq!(cpu.registers[0], 35);
 
     println!("{}", cpu.registers[0]);
 }
